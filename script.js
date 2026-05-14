ece9a7d8649e84a4322fe8300655f2a5113600f9
LuizNink <luiz.nink345@gmail.com>
2026-05-14 20:40:15 -0300

    Configuração item da lista

--- script.js
+++ script.js
@@ -5,10 +5,35 @@ const botaoAdiconar = document.getElementById('botao-adicionar')
 const listaDeCompras = document.getElementById('lista-de-compras')
 
 botaoAdiconar.addEventListener('click', () => {
-    let li = document.createElement('li')
     let nomeDigitado = nome.value
-
-    li.innerHTML = nomeDigitado
+    let precoDigitado = Number(preco.value)
+    let quantidadeDigitado = Number(quantidade.value)
+    
+    if(nomeDigitado.trim() === '') {
+        return alert('Preencha o nome antes de adicionar')
+    }
+    if(isNaN(preco) || isNaN(quantidadeDigitado)) {
+        preco.value = 0
+        quantidade.value = 0
+    }
+    
+    let li = document.createElement('li')
+    
+    li.innerHTML = 
+    `<div>
+        <div>
+            <h2>Produto: ${nomeDigitado}</h2>
+            <p>Preço: ${precoDigitado}</p>
+            <p>Quantidade: ${quantidadeDigitado}</p>
+        </div>
+        <div>
+            <button>Concluir</button>
+            <button>Excluir</button>
+        </div>
+    </div>`
     
     listaDeCompras.appendChild(li)
+    nome.value = ''
+    preco.value = ''
+    quantidade.value = ''
 })
